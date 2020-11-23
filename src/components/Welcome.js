64a1fc04d2bbd8c890a5a8fd905c71bcb51105e3
Geetanjali Katare <68340329+geetanjali2217@users.noreply.github.com>
2020-11-23 14:05:02 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
 // write code for Welcome component here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Greeting= (props) =>{
+  return (
+    <>
+  <h1>Hey!,{props.name}</h1>
+    <h2>Welcome to Newton School</h2>
+    </>
+    );
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
